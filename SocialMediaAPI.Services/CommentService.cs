b8bda8496154c79daade27f738f1e9b922f1b403
Miguel Penaloza <miguelpen02@gmail.com>
2021-08-26 18:02:50 -0400

    Merge #2

---
+++ SocialMediaAPI.Services/CommentService.cs
@@ -0,0 +1,28 @@
+﻿using SocialMediaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaAPI.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        /*public bool CreateComment(CommentCreate model)
+        {
+            var entity =
+                new CommentService()
+                {
+                    Id = _userId,
+                    Text = model.Text
+                }
+        }*/
+    }
+}
