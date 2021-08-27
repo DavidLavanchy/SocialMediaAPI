3c8395af86eac268339f52ce997446f968429608
Miguel Penaloza <miguelpen02@gmail.com>
2021-08-26 21:23:40 -0400

    Final

--- SocialMediaAPI.Services/CommentService.cs
+++ SocialMediaAPI.Services/CommentService.cs
@@ -1,4 +1,6 @@
-﻿using SocialMediaAPI.Models;
+﻿using SocialMedia.Data;
+using SocialMediaAPI.Data;
+using SocialMediaAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,14 +17,41 @@ namespace SocialMediaAPI.Services
             _userId = userId;
         }
 
-        /*public bool CreateComment(CommentCreate model)
+        public bool CreateComment(CommentCreate model)
         {
             var entity =
-                new CommentService()
+                new Comments()
                 {
-                    Id = _userId,
-                    Text = model.Text
-                }
-        }*/
+                    
+                    Text = model.Text,
+                    AuthorId = model.AuthorId
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comments.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CommentItem> GetComments()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Comments
+                        .Where(e => e.AuthorId == _userId)
+                        .Select(
+                            e =>
+                                new CommentItem
+                                {
+                                    AuthorId = e.AuthorId,
+                                    Text = e.Text
+                                }
+                               );
+                return query.ToArray();
+            }
+        }
     }
+
 }
